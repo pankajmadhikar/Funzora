b64a022e2139e8d5fed3aa7e520029c442fc03a7
pankajmadhikar <pankajmadhikar308@gmail.com>
2026-04-05 12:06:06 +0530

    removed ss creds

--- funzora_backend-main/mongoConnection.js
+++ funzora_backend-main/mongoConnection.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const config = require('./app/config');
 
 // MongoDB Connection Setup
-// mongoose.connect(`mongodb+srv://kshirsagars234:123123123@shubham.eiovjmm.mongodb.net/onlinesales`)
 const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.ob7zcad.mongodb.net/?appName=Cluster0`;
 console.log(uri); // Log the URI to ensure it's correct
 mongoose
